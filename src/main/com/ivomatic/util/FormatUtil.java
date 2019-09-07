556ea6589c192867bd9a9275a5bf8a0521377742
Marija Matic <mrj.ivancic@gmail.com>
2019-09-07 15:35:10 +0200

    streams for null values

--- src/main/com/ivomatic/util/FormatUtil.java
+++ src/main/com/ivomatic/util/FormatUtil.java
@@ -1,28 +1,24 @@
 package com.ivomatic.util;
 
-import com.ivomatic.model.Kunde;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FormatUtil {
 
-    public static String createFullName(String firstName, String middleName, String lastName) {
-        List<String> fullNameString = new ArrayList<>(Arrays.asList(firstName, middleName, lastName));
-        System.out.println(fullNameString);
-        StringBuilder fullName = new StringBuilder();
-        for (String e : fullNameString) {
-            if(e != null && !e.equals("")) {
-                fullName.append(e).append(" ");
-            }
-        }
-
-        return fullName.toString();
-
+    private static String concatStrings(String delimiter, String... strings) {
+        return Stream.of(strings)
+                .filter(s -> s != null && !s.isEmpty())
+                .collect(Collectors.joining(delimiter));
     }
 
     public static String createAddress(String street, String houseNumber, String zipCode, String city) {
-        return street + " " + houseNumber + ", " + zipCode + " " + city;
+        return concatStrings(
+                ", ",
+                concatStrings(" ", street, houseNumber),
+                concatStrings(" ", zipCode, city));
+    }
+
+    public static String createFullName(String... names) {
+        return concatStrings(" ", names);
     }
 }
